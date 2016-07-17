eba00d77518f7ad10b446e56787ae28415356745
tvcstseng <tvcstseng@gmail.com>
2016-07-17 11:02:36 +0200

    should now read temperature

--- temperature/src/main/java/nl/ttstudios/pi/gpio/temperature/TemperatureManager.java
+++ temperature/src/main/java/nl/ttstudios/pi/gpio/temperature/TemperatureManager.java
@@ -1,15 +1,33 @@
 package nl.ttstudios.pi.gpio.temperature;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import nl.ttstudios.pi.sys_io.Find;
+import nl.ttstudios.pi.util.FileReader;
 
 public class TemperatureManager {
 	private static final String baseDir = "/sys/bus/w1/devices/";
+	private static String deviceFolder;
+	private static String deviceFile;
 
 	public TemperatureManager() throws IOException {
 		String pattern = "28*";
 		List<String> filesFound = Find.find(baseDir, pattern);
+		deviceFolder = filesFound.get(0);
+		deviceFile = deviceFolder + "/w1_slave";
+
+		List<String> lines = readTemperatureRaw();
+		for (String line : lines) {
+			System.out.println(line);
+		}
+	}
+
+	public List<String> readTemperatureRaw() throws IOException {
+		Path path = Paths.get(deviceFolder, "/w1_slave");
+		return FileReader.readLines(path);
 	}
 }
---
+++ temperature/src/main/java/nl/ttstudios/pi/util/FileReader.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2012-2014 TNO Geologische Dienst Nederland
+ *
+ * Alle rechten voorbehouden.
+ * Niets uit deze software mag worden vermenigvuldigd en/of openbaar gemaakt door middel van druk, fotokopie,
+ * microfilm of op welke andere wijze dan ook, zonder voorafgaande toestemming van TNO.
+ *
+ * Indien deze software in opdracht werd uitgebracht, wordt voor de rechten en verplichtingen van opdrachtgever
+ * en opdrachtnemer verwezen naar de Algemene Voorwaarden voor opdrachten aan TNO, dan wel de betreffende
+ * terzake tussen de partijen gesloten overeenkomst.
+ */
+package nl.ttstudios.pi.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * @author T.V.C.S. Tseng
+ */
+public class FileReader {
+
+    private FileReader() {
+        // never to be used
+    }
+
+    public static byte[] read(String fileName) throws IOException {
+        String path = FileReader.class.getClassLoader().getResource( fileName ).getFile();
+        File file = new File( path );
+        fileName = file.getName();
+        return FileReader.read( file );
+    }
+
+    private static byte[] read(File file) throws IOException {
+        ByteArrayOutputStream ous = null;
+        InputStream ios = null;
+        try {
+            byte[] buffer = new byte[4096];
+            ous = new ByteArrayOutputStream();
+            ios = new FileInputStream( file );
+            int read = 0;
+            while ( ( read = ios.read( buffer ) ) != -1 ) {
+                ous.write( buffer, 0, read );
+            }
+        }
+        finally {
+            try {
+                if ( ous != null ) {
+                    ous.close();
+                }
+            }
+            catch ( IOException e ) {
+                System.err.println( e.toString() );
+            }
+
+            try {
+                if ( ios != null ) {
+                    ios.close();
+                }
+            }
+            catch ( IOException e ) {
+                System.err.println( e.toString() );
+            }
+        }
+        return ous.toByteArray();
+    }
+    
+    public static List<String> readLines(Path pathToFile) throws IOException{
+    	Charset charset = Charset.forName("ISO-8859-1");
+    	List<String> lines = Files.readAllLines(pathToFile, charset);
+		return lines;
+    }
+}
