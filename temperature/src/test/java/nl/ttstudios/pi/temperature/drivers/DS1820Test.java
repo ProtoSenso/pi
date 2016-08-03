0ca59ee139ff757a42d802c9f91d2ffe92776978
tvcstseng <tvcstseng@gmail.com>
2016-08-03 21:26:06 +0200

    initial commit

---
+++ temperature/src/test/java/nl/ttstudios/pi/temperature/drivers/DS1820Test.java
@@ -0,0 +1,14 @@
+package nl.ttstudios.pi.temperature.drivers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DS1820Test {
+
+    @Test
+    public void test() {
+        fail( "Not yet implemented" );
+    }
+
+}
