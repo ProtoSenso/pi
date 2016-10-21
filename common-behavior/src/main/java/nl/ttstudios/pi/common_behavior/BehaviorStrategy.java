f79555a0e2593724ddb97e3b8480225ffd02486f
Timothy Tseng <timothy.tseng@tno.nl>
2016-10-21 15:31:54 +0200

    Common Behavior implementation

---
+++ common-behavior/src/main/java/nl/ttstudios/pi/common_behavior/BehaviorStrategy.java
@@ -0,0 +1,10 @@
+package nl.ttstudios.pi.common_behavior;
+
+/**
+ *
+ * @author tsengtvcs
+ *
+ */
+public interface BehaviorStrategy {
+    public int execute();
+}
---
+++ common-behavior/src/main/java/nl/ttstudios/pi/common_behavior/WakeUp.java
@@ -0,0 +1,11 @@
+package nl.ttstudios.pi.common_behavior;
+
+public class WakeUp implements BehaviorStrategy {
+
+    @Override
+    public int execute() {
+        
+        return 0;
+    }
+
+}
