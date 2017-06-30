e127fa5bcf0e95ad92d3543fdd54c1f9264ba9de
Timothy Tseng <tvcstseng@gmail.com>
2017-06-30 14:56:01 +0200

    some changes

--- execute.sh
+++ execute.sh
@@ -1,1 +1,5 @@
-sudo su && cd /home/pi/development/WS_PI/pi && git checkout master && git pull WW master && ../../tools/maven/apache-maven-3.5.0/bin/mvn clean install && java -jar ./j2ee/target/j2ee-0.0.1-SNAPSHOT.jar
+sudo su
+cd /home/pi/development/WS_PI/pi
+git checkout master && git pull WW master
+../../tools/maven/apache-maven-3.5.0/bin/mvn clean install
+java -jar ./j2ee/target/j2ee-0.0.1-SNAPSHOT.jar
